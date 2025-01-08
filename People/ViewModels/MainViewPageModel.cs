ac3c2803b5860a70e155b1f01facfd7b5e31a821
Kpiiale <yuliana.capito@udla.edu.ec>
2025-01-07 23:28:07 -0500

    Boton de video

--- People/ViewModels/MainViewPageModel.cs
+++ People/ViewModels/MainViewPageModel.cs
@@ -38,11 +38,13 @@ namespace People.ViewModels
 
         public ICommand AddPersonCommand { get; }
         public ICommand GetPeopleCommand { get; }
+        public ICommand AbrirLink { get; }
 
         public MainPageViewModels()
         {
             AddPersonCommand = new Command(async () => await AddPerson());
             GetPeopleCommand = new Command(async () => await GetPeople());
+            AbrirLink = new Command(Link);
         }
 
         private async Task AddPerson()
@@ -68,6 +70,18 @@ namespace People.ViewModels
             foreach (var person in people)
             {
                 People.Add(person);
+            }
+        }
+        private async void Link()
+        {
+            var url = "https://drive.google.com/drive/folders/13s_ORHFmryvn15nskmOBeiN3q6ecIauj?usp=drive_link"; 
+            if (await Launcher.CanOpenAsync(url))
+            {
+                await Launcher.OpenAsync(url);
+            }
+            else
+            {
+                StatusMessage = "No se puede abrir el enlace.";
             }
         }
     }
