177e3cf22ca90199b339e9b5dfe0e37a7cd52659
Kpiiale <yuliana.capito@udla.edu.ec>
2025-01-07 20:48:40 -0500

    View Model

--- People/MainPage.xaml.cs
+++ People/MainPage.xaml.cs
@@ -10,22 +10,5 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
-
-    public async void OnNewButtonClicked(object sender, EventArgs args)
-    {
-        statusMessage.Text = "";
-
-        await App.PersonRepo.AddNewPerson(newPerson.Text);
-        statusMessage.Text = App.PersonRepo.StatusMessage;
-    }
-
-    public async void OnGetButtonClicked(object sender, EventArgs args)
-    {
-        statusMessage.Text = "";
-
-        List<Person> people = await App.PersonRepo.GetAllPeople();
-        peopleList.ItemsSource = people;
-    }
-
 }
 
